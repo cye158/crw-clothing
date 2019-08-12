3665820d75369b86b457ddd8b300cae80ffd57d3
Clarence Enriquez <cye0105@mail.sfsu.edu>
2019-08-12 12:49:30 -0700

    refactored homepage into components; styled menu-item components

---
+++ src/pages/homepage/homepage.component.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Directory from "../../components/directory/directory.component.js";
+import "./homepage.scss";
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
