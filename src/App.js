80883cc4b85e2cb750ed34a781ad94beae26f3d9
Clarence Enriquez <cye0105@mail.sfsu.edu>
2019-08-12 17:59:18 -0700

    update menu-items to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
-import "./App.css";
+import { Switch, Route } from "react-router-dom";
 import Homepage from "./pages/homepage/homepage.component";
+import "./App.css";
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+      </Switch>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.js
+++ src/components/menu-item/menu-item.component.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import "./menu-item.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => {
+const MenuItem = ({ title, imageUrl, size, history, match, linkUrl }) => {
+  console.log(match);
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
-        class="background-image"
+        className="background-image"
         style={{ backgroundImage: `url(${imageUrl})` }}
       />
 
@@ -17,4 +22,4 @@ const MenuItem = ({ title, imageUrl, size }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
